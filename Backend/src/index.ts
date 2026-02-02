048d550922b20d69693058c33f5edb423e61e4da
yash kumar <yashkumar.967565@gmail.com>
2026-02-02 16:50:35 +0530

    added mongo connection

---
+++ Backend/src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'Second Brain API is running!',
+  });
+});
+
+
+app.get('/health', (req: Request, res: Response) => {
+  res.json({
+    success: true,
+    message: 'Server is healthy',
+    timestamp: new Date().toISOString(),
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+  console.log(` Environment: ${process.env.NODE_ENV}`);
+});
